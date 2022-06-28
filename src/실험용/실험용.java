7e7183c442f52bef2778db910a5cbed39d92c0d9
701-08\keduit <010pink@naver.com>
2022-06-28 10:21:50 +0900

    This is commit Message!

---
+++ src/실험용/실험용.java
@@ -0,0 +1,11 @@
+package 실험용;
+import java.util.Random;
+public class 실험용 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random ran = new Random();
+		System.out.println("깃허브연습");
+	}
+
+}
